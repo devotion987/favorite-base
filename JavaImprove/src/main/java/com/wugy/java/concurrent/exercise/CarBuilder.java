54461ab56c0f8d2b667b7921611b16f2cf7d3f4d
devotion <303054578@qq.com>
2017-10-20 11:32:56 +0800

    并发包api练习

--- JavaImprove/src/main/java/com/wugy/java/concurrent/exercise/CarBuilder.java
+++ JavaImprove/src/main/java/com/wugy/java/concurrent/exercise/CarBuilder.java
@@ -2,15 +2,29 @@ package com.wugy.java.concurrent.exercise;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * wugy on 2017/10/20 11:03
  */
 public class CarBuilder {
+
+    public static void main(String[] args) throws Exception {
+        CarQueue chassisQueue = new CarQueue(),
+                finishingQueue = new CarQueue();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        RobotPool robotPool = new RobotPool();
+        exec.execute(new EngineRobot(robotPool));
+        exec.execute(new DriveTrainRobot(robotPool));
+        exec.execute(new WheelRobot(robotPool));
+        exec.execute(new Assembler(
+                chassisQueue, finishingQueue, robotPool));
+        exec.execute(new Reporter(finishingQueue));
+        // Start everything running by producing chassis:
+        exec.execute(new ChassisBuilder(chassisQueue));
+        TimeUnit.SECONDS.sleep(7);
+        exec.shutdownNow();
+    }
 }
 
 class Car {
@@ -45,8 +59,8 @@ class Car {
 
     @Override
     public synchronized String toString() {
-        return "Car " + id + " [" + " engine: " + engine + " driveTrain: " + driveTrain
-                + " wheels: " + wheels + " ]";
+        return "Car " + id + " [" + " engine: " + engine + ",driveTrain: " + driveTrain
+                + ",wheels: " + wheels + " ]";
     }
 }
 
